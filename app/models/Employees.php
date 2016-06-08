1971cab2d5bef293c6381622655c67c6dd08c99f
Mak-Di <makarov.dmitriy@gmail.com>
2016-06-08 13:40:03 +0300

    Added 'Employees' model

---
+++ app/models/Employees.php
@@ -0,0 +1,148 @@
+<?php
+
+use Phalcon\Mvc\Model\Validator\Email as Email;
+
+class Employees extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $firstName;
+
+    /**
+     *
+     * @var string
+     */
+    public $lastName;
+
+    /**
+     *
+     * @var string
+     */
+    public $position;
+
+    /**
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     *
+     * @var integer
+     */
+    public $phone;
+
+    /**
+     *
+     * @var string
+     */
+    public $note;
+
+    /**
+     *
+     * @var integer
+     */
+    public $isChief;
+
+    /**
+     *
+     * @var integer
+     */
+    public $parenId;
+
+    /**
+     * Validations and business logic
+     *
+     * @return boolean
+     */
+    public function validation()
+    {
+        $this->validate(
+            new Email(
+                array(
+                    'field'    => 'email',
+                    'required' => true,
+                )
+            )
+        );
+
+        if ($this->validationHasFailed() == true) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'employees';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Employees[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Employees
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+    /**
+     * Independent Column Mapping.
+     * Keys are the real names in the table and the values their names in the application
+     *
+     * @return array
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id',
+            'first_name' => 'firstName',
+            'last_name' => 'lastName',
+            'position' => 'position',
+            'email' => 'email',
+            'phone' => 'phone',
+            'note' => 'note',
+            'is_chief' => 'isChief',
+            'parent_id' => 'parentId'
+        );
+    }
+
+    /**
+     * Get children
+     * 
+     * @param int $parent_id
+     * @return mixed
+     */
+    public static function getChildren($parent_id = 0)
+    {
+        return parent::findByParentId($parent_id);
+    }
+}
